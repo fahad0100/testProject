2d9bd56b742da0bd2f0793973ceb04915baa761b
TestAuthor <exaple@gmail.com>
2022-01-25 04:04:04 +0300

    here

--- SaveWord/test/ClassA.swift
+++ SaveWord/test/ClassA.swift
@@ -14,6 +14,7 @@ class ClassA: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        print("here")
    
         // Do any additional setup after loading the view.
     }
